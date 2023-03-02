42167905faf2f5dea2971168e0d7b023cf21a987
mihai@nivasoftware.ro <mihai@nivasoftware.ro>
2023-03-02 10:29:26 +0200

    clean home page

--- src/Niva.Erp.Web.Host/src/app/home/home.component.ts
+++ src/Niva.Erp.Web.Host/src/app/home/home.component.ts
@@ -2,8 +2,6 @@ import { Component, Injector, ChangeDetectionStrategy } from '@angular/core';
 import { AppComponentBase } from '@shared/app-component-base';
 import { appModuleAnimation } from '@shared/animations/routerTransition';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
-import { FiledocSearchComponent } from '../shared/modal/filedoc-search/filedoc-search.component';
-import { NotificareDto, NotificareServiceProxy, PaapServiceProxy } from "../../shared/service-proxies/service-proxies";
 
 @Component({
   templateUrl: './home.component.html',
@@ -13,82 +11,15 @@ import { NotificareDto, NotificareServiceProxy, PaapServiceProxy } from "../../s
 
 export class HomeComponent extends AppComponentBase {
 
-    selectedId: number;
-    invoicesWithoutPaapCount: number;
-    nefinalizat30zilePaapListCount: number;
-    nefinalizatDepasitPaapListCount: number;
-    notificariList: NotificareDto[] = [];
-
+    
     constructor(injector: Injector,
-        private _modalService: BsModalService,
-        private _paapService: PaapServiceProxy,
-        private _notificareService: NotificareServiceProxy) {
+        private _modalService: BsModalService) {
         super(injector);
     }
 
     ngOnInit() {
-        if (this.isGranted('Buget.PAAP.AlocareFacturi')) {
-            this._paapService.getInvoiceDetailsWithoutPaapCount().subscribe(result => {
-                this.invoicesWithoutPaapCount = result
-            });
-        }
-
-        if (this.isGranted('Buget.PAAP.Super')) {
-            this._paapService.nefinalizat30zilePaapListCount().subscribe(result => {
-                this.nefinalizat30zilePaapListCount = result
-            });
-        }
-
-        if (this.isGranted('Buget.PAAP.Super')) {
-            this._paapService.nefinalizatDepasitPaapListCount().subscribe(result => {
-                this.nefinalizatDepasitPaapListCount = result
-            });
-        }
-        this.getNotificariList();
+        
     }
 
-    filedocsearch(): void {
-        this.showFildocSearchDialog();
-    }
-
-    private showFildocSearchDialog(id?: number): void {
-        let createOrEditUserDialog: BsModalRef;
-        if (!id) {
-            createOrEditUserDialog = this._modalService.show(
-                FiledocSearchComponent,
-                {
-                    class: 'modal-lg',
-                }
-            );
-        } else {
-            createOrEditUserDialog = this._modalService.show(
-                FiledocSearchComponent,
-                {
-                    class: 'modal-lg',
-                    initialState: {
-                        id: id,
-                    },
-                }
-            );
-        }
-
-        createOrEditUserDialog.content.onSave.subscribe((id) => {
-            this.refresh(id);
-        });
-    }
-    refresh(id) {
-        this.selectedId = id;
-    }
-
-    getNotificariList() {
-        this._notificareService.getNotificareList().subscribe(result => {
-            this.notificariList = result;
-        });
-    }
-
-    updateStareNotificare(notificareId: number) {
-        this._notificareService.updateStareNotificare(notificareId).subscribe(result => {
-            this.getNotificariList();
-        });
-    }
+    
 }
